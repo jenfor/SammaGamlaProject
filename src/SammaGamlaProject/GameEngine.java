e2e8eb637913ba855bf52a1ae6d3fc1c0ba9d445
jenfor <jenfor@student.chalmers.se>
2015-02-16 13:46:55 +0100

    Ny okörbar
    uppdaterad mitt under arbete

--- src/SammaGamlaProject/GameEngine.java
+++ src/SammaGamlaProject/GameEngine.java
@@ -29,7 +29,7 @@ public class GameEngine {
 		return myFrame;
 	}
 	
-	public String getCurrentRoom() {
+	public String getCurrent() {
 		return current;
 	}
 	
--- src/SammaGamlaProject/UserInterface.java
+++ src/SammaGamlaProject/UserInterface.java
@@ -1,25 +1,52 @@
 package SammaGamlaProject;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.Image;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+
+import Projektet.GameEngine;
+import Projektet.JPanelWithBackground;
+import Projektet.PanelSklett;
+import Projektet.Room;
 
 /**
  * Hej! Jag har flyttat ut createMenu som en egen metod. Tycker att det blir finare och kortare pÃ¥ createGUI dÃ¥ ^ ^
  */
 public class UserInterface {
-	private GameEngine engine;
-    private JFrame myFrame;
+	  private GameEngine engine;
+	    private JFrame myFrame;
+	    private JTextField entryField;
+	    private JTextArea log;
+	    private JLabel image;
+	   	    private JButton exitButton;
+	    private HashMap<String,JButton> exitButtons = new HashMap<String,JButton>();
+	    private JPanelWithBackground panel;
+	    private Room room;
+	    public PanelSklett invisPanels;
+	    private JPanel invisPanel;
 	
 	//Den metoden som skapas i game
-	public UserInterface(GameEngine gameEngine) {
-		engine = gameEngine;
-		myFrame = engine.frame();
-	}
+    public UserInterface(GameEngine gameEngine)
+    {
+        engine = gameEngine;
+        createGUI();
+        room = new Room(engine.getCurrent());
+        invisPanels = new PanelSklett(engine);
+    }
+	
 	
 	public void createMenu() {
     	//Where the GUI is created:
@@ -156,7 +183,120 @@ public class UserInterface {
         myFrame.pack();
 	}
 	
-	public void createGUI() {
-		
-	}
+	 public void createGUI()
+	    {
+	        myFrame = new JFrame("Spel");
+	        entryField = new JTextField(34);
+	        exitButton = new JButton("Exit");
+	        JButton button2 = new JButton("Affär");
+	        JButton mapButton = new JButton("Karta");
+	        JButton button4 = new JButton("Föremål");
+	        JButton button5 = new JButton("Pengar");
+	        JButton button6 = new JButton("Föremål");
+	        JButton button7 = new JButton("Pengar");
+	        //image = room.getPicture(current);
+	        File sourceimage = new File("pictures/stig.jpg");//Bilder ska egentligen ligga sparade i rummen 
+	        Image image= null;								// Just nu står det här
+			try {
+				image = ImageIO.read(sourceimage);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	        
+	                
+	        
+	        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+	        double width = screenSize.getWidth();
+	        double height = screenSize.getHeight();
+	        
+	        double textHeight = height * 0.15;
+	        double textWidth = height * 0.15;
+	        double imgWidth = width * 0.9;
+	        double imgHeight = height * 0.9;
+	        
+	        
+	        myFrame.setPreferredSize(new Dimension((int)width, (int)height));
+	        myFrame.setMinimumSize(new Dimension((int)width, (int)height));
+	        myFrame.setResizable(false);
+	        myFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+	        
+	        
+	       
+	        panel = new JPanelWithBackground(image); 
+			
+			
+	        
+	        JPanel p = new JPanel(new GridLayout(4,1));
+	        JPanel p2 = new JPanel(new GridLayout(4,1));
+	        JPanel b = new JPanel();
+	        b.setLayout(new BoxLayout(b, BoxLayout.X_AXIS));
+
+	        p.add(button2);
+	        p.add(mapButton);
+	        p.add(exitButton);
+	        b.add(button4);
+	        b.add(button5);
+	        
+	        panel.setLayout(new BorderLayout());
+	        //panel.add(textBox, BorderLayout.AFTER_LAST_LINE);
+	        panel.add(p, BorderLayout.WEST);
+	        panel.add(p2, BorderLayout.EAST);
+	        panel.add(b, BorderLayout.NORTH);
+	       
+	        
+	        panel.setPreferredSize(new Dimension((int)width, (int)height)); //bildstorlek, gör om till att skala
+	        panel.setMinimumSize(new Dimension((int)width, (int)height)); //istället för att skära av
+	        
+	        myFrame.getContentPane().add(panel, BorderLayout.NORTH);
+	        
+	        button2.addActionListener(new ActionListener() {
+	            @Override
+	            public void actionPerformed(ActionEvent event) {
+	                System.exit(0);
+	            }
+	        });
+	        
+	        exitButton.addActionListener(new ActionListener() {
+	            @Override
+	            public void actionPerformed(ActionEvent event) {
+	            	System.exit(0);
+	            }
+	        });
+	        
+	        mapButton.addActionListener(new ActionListener() {
+	            @Override
+	            public void actionPerformed(ActionEvent event) {
+	            	System.exit(0);
+	            }
+	        });
+	        
+	         
+	               	        
+	        myFrame.pack();
+	        myFrame.setVisible(true);
+	        entryField.requestFocus();
+	        
+	        changeRoom("Center");
+	        
+	    }
+	 
+	 private void setJPanelWithBackground(image)
+	 {
+		  image = i;
+		 
+	 }
+	 
+	 private void changeRoom(String current)
+	 {
+		 if(current.equals("Center")) room = invisPanels.center; 
+			if(current.equals("Shop")) room = invisPanels.shop;
+			if(current.equals("Garden")) room = invisPanels.garden;
+			else room = invisPanels.miniGame1;
+			
+		setJPanelWithBackground(room.getPicture());
+		 panel.add(room.getRoomPanel(), BorderLayout.NORTH);
+		 
+	 }
+
 }
